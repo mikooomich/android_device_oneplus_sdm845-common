b350b002a38b4d42fbfdd4c6545194d9acc6b577
mikooomich <mikooomichael@gmail.com>
2024-09-13 00:10:43 -0400

    DNM: Personal build feature

--- common.mk
+++ common.mk
@@ -456,3 +456,19 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+
+# Inline Gapps
+include vendor/gapps/arm64/arm64-vendor.mk
+
+# I hate Safety Net
+PRODUCT_PACKAGES += \
+    ih8sn
+
+DEVICE := $(subst lineage_,,$(TARGET_PRODUCT))
+IH8SN_CONFIG_PATH := $(LOCAL_PATH)/prebuilt/etc/ih8sn_$(DEVICE).conf
+
+ifneq ($(wildcard $(IH8SN_CONFIG_PATH)),)
+PRODUCT_COPY_FILES += \
+    $(IH8SN_CONFIG_PATH):/system/etc/ih8sn.conf
+endif
